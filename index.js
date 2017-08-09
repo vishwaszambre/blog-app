b3f66d2e6be65594cb1f3eccd6710829bf552966
vishwas-zambre <vishwas.zambre@gmail.com>
2017-08-09 12:50:25 +0530

    server-set-up-done

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.uri, (err) => {
+    if (err) {
+        console.log('Could not connect to database: ', err);
+    } else {
+        console.log('Connected to database: ' + config.db);
+    }
+});
+
+app.use(express.static(__dirname+'/client/dist/'));
+
+app.get('*', (req, res) => {
+    res.send('<h1>hello world!</h1>');
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
